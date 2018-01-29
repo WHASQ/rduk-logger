014b9295b6f5f42f6c0f680dfab318a1ff67688c
Kim Ung <k.ung@rduk.fr>
2018-01-29 15:17:41 +0100

    V1.0.0 (#4)
    * 1.0.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,18 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
+## 1.0.0 - 2018-01-29
+### Added
+- adopt [`standard`](https://standardjs.com/) code style
+- add `greenkeeper` badge
+### Changed
+- fix minimal node version to 6.4.0
+- use es6 syntax
+- update `@rduk/configuration` to latest version (2.1.1)
+- update `@rduk/provider` to latest version (3.0.1)
+- update `jasmine` to latest version (2.9.0)
+- raise code coverage to 100%
+
 ## 0.1.5 - 2018-01-10
 ### Changed
 - update `@rduk/configuration` to latest version (2.1.0)
--- spec/loggerSpec.js
+++ spec/loggerSpec.js
@@ -1,18 +1,18 @@
 /**
  * MIT License
- * 
+ *
  * Copyright (c) 2017 RDUK <tech@rduk.fr>
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -22,41 +22,47 @@
  * SOFTWARE.
  */
 
-'use strict';
+/* eslint-env jasmine */
 
-describe('logger', function() {
+'use strict'
 
-    let levels = ['error', 'warn', 'info', 'verbose', 'debug'];
+describe('logger', function () {
+  let levels = ['error', 'warn', 'info', 'verbose', 'debug']
 
-    describe('base provider', function() {
+  describe('base provider', function () {
+    const Base = require('../lib/provider/base')
+    let logger = new Base({name: 'default', providers: []})
 
-        const Base = require('../lib/provider/base');
-        let logger = new Base({name: 'default', providers: []});
+    describe('log methods', function () {
+      it('should thrown NotImplementedError', function () {
+        levels.forEach(function (level) {
+          expect(function () {
+            logger[level]()
+          }).toThrowError()
+        })
+      })
+    })
+  })
 
-        describe('log methods', function() {
-            it('should thrown NotImplementedError', function() {
-                levels.forEach(function(level) {
-                    expect(function() {
-                        logger[level]();
-                    }).toThrowError();
-                });
-            });
-        });
+  describe('log methods', function () {
+    const logger = require('../lib')
 
-    });
+    it('should have been called', function () {
+      levels.forEach(function (level) {
+        spyOn(logger, level).and.callThrough()
+        logger[level]('test')
+        expect(logger[level]).toHaveBeenCalled()
+      })
+    })
+  })
 
-    describe('log methods', function() {
-    
-        const logger = require('../lib');
+  describe('section property, without config', function () {
+    const logger = require('../lib')
 
-        it('should have been called', function() {
-            levels.forEach(function(level) {
-                spyOn(logger, level).and.callThrough();
-                logger[level]('test');
-                expect(logger[level]).toHaveBeenCalled();
-            });
-            
-        });
-    });
-
-});
+    it('should return the default one', function () {
+      spyOn(logger, 'getConfiguration').and.returnValue(null)
+      logger.flush()
+      expect(logger.section).toBeDefined()
+    })
+  })
+})
